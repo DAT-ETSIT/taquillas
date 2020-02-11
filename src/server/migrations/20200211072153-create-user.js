d5972e65bf9f9e3acd73b3039446d6f06c2982ed
Enrique Pelaez <e.pelaez@alumnos.upm.es>
2020-02-11 17:19:57 +0100

    Model and migration of Users

---
+++ src/server/migrations/20200211072153-create-user.js
@@ -0,0 +1,41 @@
+
+module.exports = {
+	up: (queryInterface, Sequelize) => queryInterface.createTable('Users', {
+		id: {
+			allowNull: false,
+			autoIncrement: true,
+			primaryKey: true,
+			type: Sequelize.INTEGER,
+		},
+		name: {
+			type: Sequelize.STRING,
+			allowNull: false,
+		},
+		phone: {
+			type: Sequelize.STRING,
+			allowNull: false,
+		},
+		dni: {
+			type: Sequelize.STRING,
+		},
+		email: {
+			type: Sequelize.STRING,
+			allowNull: false,
+			unique: true,
+		},
+		isAdmin: {
+			type: Sequelize.BOOLEAN,
+			allowNull: false,
+			defaultValue: false,
+		},
+		createdAt: {
+			allowNull: false,
+			type: Sequelize.DATE,
+		},
+		updatedAt: {
+			allowNull: false,
+			type: Sequelize.DATE,
+		},
+	}),
+	down: (queryInterface) => queryInterface.dropTable('Users'),
+};
