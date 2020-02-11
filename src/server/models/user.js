20670870bfd388f97413ab2426b83119ea6c705c
Enrique Pelaez <e.pelaez@alumnos.upm.es>
2020-02-11 17:19:57 +0100

    Model and migration of Users

---
+++ src/server/models/user.js
@@ -0,0 +1,16 @@
+
+module.exports = (sequelize, DataTypes) => {
+	const User = sequelize.define('User', {
+		name: DataTypes.STRING,
+		phone: DataTypes.STRING,
+		dni: DataTypes.STRING,
+		email: DataTypes.STRING,
+		isAdmin: DataTypes.BOOLEAN,
+	}, {});
+	User.associate = function associate(models) {
+		models.User.hasMany(models.Event, { foreignKey: 'userId' });
+		models.User.hasMany(models.Rental, { foreignKey: 'userId' });
+		// More associations can be defined here
+	};
+	return User;
+};
