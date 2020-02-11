c8aad62e2b69c262f3ad897020b24ac05511bb70
Enrique Pelaez <e.pelaez@alumnos.upm.es>
2020-02-11 17:18:55 +0100

    Model and migration of Event Types

---
+++ src/server/migrations/20200211072053-create-event-type.js
@@ -0,0 +1,25 @@
+
+module.exports = {
+	up: (queryInterface, Sequelize) => queryInterface.createTable('EventTypes', {
+		id: {
+			allowNull: false,
+			autoIncrement: true,
+			primaryKey: true,
+			type: Sequelize.INTEGER,
+		},
+		name: {
+			type: Sequelize.STRING,
+			allowNull: false,
+			unique: true,
+		},
+		createdAt: {
+			allowNull: false,
+			type: Sequelize.DATE,
+		},
+		updatedAt: {
+			allowNull: false,
+			type: Sequelize.DATE,
+		},
+	}),
+	down: (queryInterface) => queryInterface.dropTable('EventTypes'),
+};
---
+++ src/server/models/eventtype.js
@@ -0,0 +1,11 @@
+
+module.exports = (sequelize, DataTypes) => {
+	const EventType = sequelize.define('EventType', {
+		name: DataTypes.STRING,
+	}, {});
+	EventType.associate = function associate(models) {
+		models.EventType.hasMany(models.Event, { foreignKey: 'eventTypeId' });
+		// More associations can be defined here
+	};
+	return EventType;
+};
