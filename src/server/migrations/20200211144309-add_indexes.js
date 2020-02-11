c9a1699fea459aa93bb4b3f60a82a6b2ecb49efb
Enrique Pelaez <e.pelaez@alumnos.upm.es>
2020-02-11 17:42:23 +0100

    Add indexes to certain fields
    Add indexes to the 'email' and 'dni' fields in order to optimize searches of specific users.

---
+++ src/server/migrations/20200211144309-add_indexes.js
@@ -0,0 +1,20 @@
+
+module.exports = {
+	up: (queryInterface) => queryInterface.addIndex(
+		'Users',
+		['dni'],
+	)
+		.then(() => queryInterface.addIndex(
+			'Users',
+			['phone'],
+		)),
+
+	down: (queryInterface) => queryInterface.removeIndex(
+		'Users',
+		['dni'],
+	)
+		.then(() => queryInterface.removeIndex(
+			'Users',
+			['phone'],
+		)),
+};
