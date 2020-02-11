0af5459c8416cc1792babffc56196f2c5a5adb79
Enrique Pelaez <e.pelaez@alumnos.upm.es>
2020-02-11 17:15:22 +0100

    Model and migration of Locations

---
+++ src/server/migrations/20200211071905-create-location.js
@@ -0,0 +1,29 @@
+
+module.exports = {
+	up: (queryInterface, Sequelize) => queryInterface.createTable('Locations', {
+		id: {
+			allowNull: false,
+			autoIncrement: true,
+			primaryKey: true,
+			type: Sequelize.INTEGER,
+		},
+		name: {
+			type: Sequelize.STRING,
+			allowNull: false,
+			unique: true,
+		},
+		description: {
+			type: Sequelize.TEXT,
+			allowNull: false,
+		},
+		createdAt: {
+			allowNull: false,
+			type: Sequelize.DATE,
+		},
+		updatedAt: {
+			allowNull: false,
+			type: Sequelize.DATE,
+		},
+	}),
+	down: (queryInterface) => queryInterface.dropTable('Locations'),
+};
---
+++ src/server/models/location.js
@@ -0,0 +1,12 @@
+
+module.exports = (sequelize, DataTypes) => {
+	const Location = sequelize.define('Location', {
+		name: DataTypes.STRING,
+		description: DataTypes.TEXT,
+	}, {});
+	Location.associate = function associate(models) {
+		models.Location.hasMany(models.Locker, { foreignKey: 'locationId' });
+		// More associations can be defined here
+	};
+	return Location;
+};
