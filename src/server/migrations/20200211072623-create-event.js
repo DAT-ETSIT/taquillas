5d2396d788d1417be4951597b3a74a9b5eba8b0c
Enrique Pelaez <e.pelaez@alumnos.upm.es>
2020-02-11 17:31:47 +0100

    Model and migration of Events

---
+++ src/server/migrations/20200211072623-create-event.js
@@ -0,0 +1,63 @@
+
+module.exports = {
+	up: (queryInterface, Sequelize) => queryInterface.createTable('Events', {
+		id: {
+			allowNull: false,
+			autoIncrement: true,
+			primaryKey: true,
+			type: Sequelize.INTEGER,
+		},
+		description: {
+			type: Sequelize.TEXT,
+		},
+		eventTypeId: {
+			type: Sequelize.INTEGER,
+			references: {
+				model: 'EventTypes',
+				key: 'id',
+			},
+			onDelete: 'SET NULL',
+		},
+		userId: {
+			type: Sequelize.INTEGER,
+			references: {
+				model: 'Users',
+				key: 'id',
+			},
+			onDelete: 'SET NULL',
+		},
+		lockerId: {
+			type: Sequelize.INTEGER,
+			references: {
+				model: 'Lockers',
+				key: 'id',
+			},
+			onDelete: 'SET NULL',
+		},
+		paymentId: {
+			type: Sequelize.INTEGER,
+			references: {
+				model: 'Payments',
+				key: 'id',
+			},
+			onDelete: 'SET NULL',
+		},
+		rentalId: {
+			type: Sequelize.INTEGER,
+			references: {
+				model: 'Rentals',
+				key: 'id',
+			},
+			onDelete: 'SET NULL',
+		},
+		createdAt: {
+			allowNull: false,
+			type: Sequelize.DATE,
+		},
+		updatedAt: {
+			allowNull: false,
+			type: Sequelize.DATE,
+		},
+	}),
+	down: (queryInterface) => queryInterface.dropTable('Events'),
+};
---
+++ src/server/models/event.js
@@ -0,0 +1,20 @@
+
+module.exports = (sequelize, DataTypes) => {
+	const Event = sequelize.define('Event', {
+		description: DataTypes.TEXT,
+		eventTypeId: DataTypes.INTEGER,
+		userId: DataTypes.INTEGER,
+		lockerId: DataTypes.INTEGER,
+		paymentId: DataTypes.INTEGER,
+		rentalId: DataTypes.INTEGER,
+	}, {});
+	Event.associate = function associate(models) {
+		models.Event.belongsTo(models.EventType, { foreignKey: 'eventTypeId' });
+		models.Event.belongsTo(models.User, { foreignKey: 'userId' });
+		models.Event.belongsTo(models.Locker, { foreignKey: 'lockerId' });
+		models.Event.belongsTo(models.Rental, { foreignKey: 'rentalId' });
+		models.Event.belongsTo(models.Payment, { foreignKey: 'paymentId' });
+		// More associations can be defined here
+	};
+	return Event;
+};
