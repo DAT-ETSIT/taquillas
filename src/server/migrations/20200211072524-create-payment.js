76ae447c07289dd051d9eb1d6ea4c47827878a3c
Enrique Pelaez <e.pelaez@alumnos.upm.es>
2020-02-11 17:28:28 +0100

    Model and migration of Payments

---
+++ src/server/migrations/20200211072524-create-payment.js
@@ -0,0 +1,48 @@
+
+module.exports = {
+	up: (queryInterface, Sequelize) => queryInterface.createTable('Payments', {
+		id: {
+			allowNull: false,
+			autoIncrement: true,
+			primaryKey: true,
+			type: Sequelize.INTEGER,
+		},
+		quantity: {
+			type: Sequelize.DECIMAL,
+			allowNull: false,
+		},
+		userId: {
+			type: Sequelize.INTEGER,
+			references: {
+				model: 'Users',
+				key: 'id',
+			},
+			onDelete: 'CASCADE',
+		},
+		rentalId: {
+			type: Sequelize.INTEGER,
+			references: {
+				model: 'Rentals',
+				key: 'id',
+			},
+			onDelete: 'SET NULL',
+		},
+		paymentMethodId: {
+			type: Sequelize.INTEGER,
+			references: {
+				model: 'PaymentMethods',
+				key: 'id',
+			},
+			onDelete: 'SET NULL',
+		},
+		createdAt: {
+			allowNull: false,
+			type: Sequelize.DATE,
+		},
+		updatedAt: {
+			allowNull: false,
+			type: Sequelize.DATE,
+		},
+	}),
+	down: (queryInterface) => queryInterface.dropTable('Payments'),
+};
---
+++ src/server/models/payment.js
@@ -0,0 +1,17 @@
+
+module.exports = (sequelize, DataTypes) => {
+	const Payment = sequelize.define('Payment', {
+		quantity: DataTypes.DECIMAL,
+		userId: DataTypes.INTEGER,
+		rentalId: DataTypes.INTEGER,
+		paymentMethodId: DataTypes.INTEGER,
+	}, {});
+	Payment.associate = function associate(models) {
+		models.Payment.belongsTo(models.User, { foreignKey: 'userId', onDelete: 'CASCADE' });
+		models.Payment.belongsTo(models.Rental, { foreignKey: 'rentalId' });
+		models.Payment.belongsTo(models.PaymentMethodId, { foreignKey: 'paymentMethodId' });
+		models.Payment.hasMany(models.Event, { foreignKey: 'paymentId' });
+		// More associations can be defined here
+	};
+	return Payment;
+};
