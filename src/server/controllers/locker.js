df10a36235e04be554f354283cd36b657f681daa
Enrique Pelaez <e.pelaez@alumnos.upm.es>
2020-02-21 15:51:50 +0100

    Basic implementation of the CRUD functions for the lockers.

--- src/server/controllers/locker.js
+++ src/server/controllers/locker.js
@@ -27,3 +27,81 @@ exports.load = (req, res, next, lockerId) => {
 		.catch((error) => next(error));
 };
 
+exports.index = (req, res, next) => {
+	const options = {
+		include: [
+			models.Location,
+			models.Status,
+			{
+				model: models.Loan,
+				where: { isActive: 1 },
+				include: [models.User],
+			},
+		],
+	};
+	models.Locker.findAll(options)
+		.then((lockers) => {
+			res.json(lockers);
+		})
+		.catch((error) => next(error));
+};
+
+exports.show = (req, res) => {
+	const { locker } = req;
+	res.json(locker);
+};
+
+exports.create = (req, res, next) => {
+	const locker = models.Locker.build(
+		{
+			lockerNumber: req.body.lockerNumber,
+			statusId: req.body.statusId,
+			locationId: req.body.locationId,
+		},
+	);
+	locker.save()
+		.then(() => {
+			res.json({ status: 'OK' });
+		})
+		.catch(Sequelize.ValidationError, (error) => {
+			res.status(400).json({ status: 'ERROR', errors: error.errors });
+		})
+		.catch((error) => {
+			next(error);
+		});
+};
+
+exports.update = (req, res, next) => {
+	const { locker, body } = req;
+	const fields = [];
+	const allowedFields = ['lockerNumber', 'statusId', 'locaitonId'];
+
+	// Check if there is at least one field to update
+	allowedFields.forEach((field) => {
+		if (Object.prototype.hasOwnProperty.call(body, field)) {
+			locker[field] = body[field];
+			fields.push(field);
+		}
+	});
+	// Check if there is at least one field to update
+	if (!fields) {
+		res.status(400).json({ status: 'ERROR', errors: ['No fields to update'] });
+	}
+
+	locker.save({ fields })
+		.then(() => {
+			res.json({ status: 'OK' });
+		})
+		.catch(Sequelize.ValidationError, (error) => {
+			res.status(400).json({ status: 'ERROR', errors: error.errors });
+		})
+		.catch((error) => next(error));
+};
+
+exports.destroy = (req, res, next) => {
+	req.locker.destroy()
+		.then(() => {
+			req.json({ status: 'OK' });
+		})
+		.catch((error) => next(error));
+};
--- src/server/routers/dashboard.js
+++ src/server/routers/dashboard.js
@@ -6,4 +6,11 @@ const router = express.Router();
 // Autoload for routes using :param
 router.param('lockerId', lockerController.load);
 
+// Routes for the resource /lockers
+router.get('/lockers', lockerController.index);
+router.get('/locker/:lockerId(\\d+)', lockerController.show);
+router.post('/locker', lockerController.create);
+router.put('/locker/:lockerId(\\d+)', lockerController.update);
+router.delete('/locker/:lockerId(\\d+)', lockerController.destroy);
+
 module.exports = router;
