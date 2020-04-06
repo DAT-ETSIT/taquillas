27a6d914e681a818cf324014c4aa35c672c37f87
Enrique Peláez Cornejo <e.pelaez@alumnos.upm.es>
2020-04-06 10:34:05 +0200

    Routes for the model 'location'

--- src/server/routers/admin.js
+++ src/server/routers/admin.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const lockerController = require('../controllers/locker');
 const lockerStateController = require('../controllers/lockerState');
+const locationController = require('../controllers/location');
 
 const router = express.Router();
 
 // Autoload for routes using :param
 router.param('lockerId', lockerController.load);
 router.param('lockerStateId', lockerStateController.load);
+router.param('locationId', locationController.load);
 
 // Routes for the model locker
 router.get('/lockers', lockerController.index);
@@ -21,5 +23,12 @@ router.get('/lockerState/:lockerStateId(\\d+)', lockerStateController.show);
 router.post('/lockerState', lockerStateController.create);
 router.put('/lockerState/:lockerStateId(\\d+)', lockerStateController.update);
 router.delete('/lockerState/:lockerStateId(\\d+)', lockerStateController.destroy);
+
+// Routes for the model location
+router.get('/locations', locationController.index);
+router.get('/location/:locationId(\\d+)', locationController.show);
+router.post('/location', locationController.create);
+router.put('/location/:locationId(\\d+)', locationController.update);
+router.delete('/location/:locationId(\\d+)', locationController.destroy);
 
 module.exports = router;
