507f3f9698a979ba023c8dbd7c9616fd58621da9
Enrique Peláez Cornejo <e.pelaez@alumnos.upm.es>
2020-04-06 10:28:27 +0200

    Routes for the model 'locker'

--- src/server/routers/admin.js
+++ src/server/routers/admin.js
@@ -1,5 +1,16 @@
 const express = require('express');
+const lockerController = require('../controllers/locker');
 
 const router = express.Router();
+
+// Autoload for routes using :param
+router.param('lockerId', lockerController.load);
+
+// Routes for the model locker
+router.get('/lockers', lockerController.index);
+router.get('/locker/:lockerId(\\d+)', lockerController.show);
+router.post('/locker', lockerController.create);
+router.put('/locker/:lockerId(\\d+)', lockerController.update);
+router.delete('/locker/:lockerId(\\d+)', lockerController.destroy);
 
 module.exports = router;
