0ea141c7413462c6c3ee31f3ff67d3df82221b49
Enrique Peláez Cornejo <e.pelaez@alumnos.upm.es>
2020-04-07 11:45:34 +0200

    Update server.js with the new server configuration file

--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,11 @@
+const env = process.env.NODE_ENV || 'development';
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const router = require('./router');
 const adminRouter = require('./routers/admin');
 const appRouter = require('./routers/app');
-const config = require('./config.json');
+const config = require('./config/server.json')[env];
 const { globalErrorHandler } = require('./errors');
 
 const app = express();
@@ -17,7 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 // Serve the frontend files.
 app.use(express.static('dist'));
 // Let Express know if we are using a reverse proxy.
-if (config.server.usingProxy) app.set('trust proxy', 1);
+if (config.usingProxy) app.set('trust proxy', 1);
 // Main API router.
 app.use('/api/v1/app', appRouter);
 app.use('/api/v1/admin', adminRouter);
@@ -27,7 +28,7 @@ app.use('*', (req, res) => res.sendFile(path.join(__dirname, '../../dist/index.h
 // The error handler that produces 404/500 HTTP responses.
 app.use(globalErrorHandler);
 
-app.listen(config.server.port, function () { // eslint-disable-line func-names
+app.listen(config.port, function () { // eslint-disable-line func-names
 	console.log('TAQUILLAS - Running on', this.address().address,
 		'port', this.address().port);
 });
