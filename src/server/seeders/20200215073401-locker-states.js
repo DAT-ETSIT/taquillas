d309635cdc980631b0ad231704d28fa85d505958
Enrique Peláez Cornejo <e.pelaez@alumnos.upm.es>
2020-04-07 08:11:53 +0200

    Add new hardcoded ids for the states of the lockers and rentals.

--- src/server/seeders/20200215073401-locker-states.js
+++ src/server/seeders/20200215073401-locker-states.js
@@ -1,25 +1,30 @@
+const { LockerStates } = require('../constants');
 
 module.exports = {
 	up: (queryInterface) => queryInterface.bulkInsert('LockerStates', [
 		{
+			id: LockerStates.UNAVAILABLE,
 			name: 'Unavailable',
 			description: 'Locker not available for rent',
 			createdAt: new Date(),
 			updatedAt: new Date(),
 		},
 		{
+			id: LockerStates.AVAILABLE,
 			name: 'Available',
 			description: 'Locker available for rent',
 			createdAt: new Date(),
 			updatedAt: new Date(),
 		},
 		{
+			id: LockerStates.RESERVED,
 			name: 'Reserved',
 			description: 'Locker reserved for a user',
 			createdAt: new Date(),
 			updatedAt: new Date(),
 		},
 		{
+			id: LockerStates.RENTED,
 			name: 'Rented',
 			description: 'Locker rented to a user',
 			createdAt: new Date(),
