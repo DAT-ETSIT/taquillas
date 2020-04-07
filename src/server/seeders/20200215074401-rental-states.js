69384df5c91f933fab9f900aeeab84aa2ca9ce05
Enrique Peláez Cornejo <e.pelaez@alumnos.upm.es>
2020-04-07 08:11:53 +0200

    Add new hardcoded ids for the states of the lockers and rentals.

--- src/server/seeders/20200215074401-rental-states.js
+++ src/server/seeders/20200215074401-rental-states.js
@@ -1,37 +1,44 @@
+const { RentalStates } = require('../constants');
 
 module.exports = {
 	up: (queryInterface) => queryInterface.bulkInsert('RentalStates', [
 		{
+			id: RentalStates.REQUESTED,
 			name: 'Requested',
 			description: 'The user has requested a locker. Petition pending approval by administrators.',
 			createdAt: new Date(),
 			updatedAt: new Date(),
 		},
 		{
+			id: RentalStates.RESERVED,
 			name: 'Reserved',
 			description: 'The administration has approved the request. The user has to pay the deposit and the price of the locker.',
 			createdAt: new Date(),
 			updatedAt: new Date(),
 		},
 		{
+			id: RentalStates.RENTED,
 			name: 'Rented',
 			description: 'Rental is active until the expiration date.',
 			createdAt: new Date(),
 			updatedAt: new Date(),
 		},
 		{
+			id: RentalStates.CLAIMED,
 			name: 'Claimed',
 			description: 'The administration claimed the return of the keys in order to end the rental.',
 			createdAt: new Date(),
 			updatedAt: new Date(),
 		},
 		{
+			id: RentalStates.RENEW_REQUESTED,
 			name: 'Renew requested',
 			description: 'The user has requested the renewal of the rental.',
 			createdAt: new Date(),
 			updatedAt: new Date(),
 		},
 		{
+			id: RentalStates.RETURNED,
 			name: 'Returned',
 			description: 'The rental of the locker is over, the keys and the deposit have been returned.',
 			createdAt: new Date(),
