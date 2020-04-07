55c6487186a7ae0b779c82bfab47e858e78c9461
Enrique Peláez Cornejo <e.pelaez@alumnos.upm.es>
2020-04-07 08:12:24 +0200

    Constants file with the rental and locker states.

---
+++ src/server/constants.js
@@ -0,0 +1,15 @@
+exports.RentalStates = {
+	REQUESTED: 1,
+	RESERVED: 2,
+	RENTED: 3,
+	CLAIMED: 4,
+	RENEW_REQUESTED: 5,
+	RETURNED: 6,
+};
+
+exports.LockerStates = {
+	UNAVAILABLE: 1,
+	AVAILABLE: 2,
+	RESERVED: 3,
+	RENTED: 4,
+};
