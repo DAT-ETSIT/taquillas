9b715996f1e0f1056b75f76274a0f9c5a7d30719
Enrique Pelaez <e.pelaez@alumnos.upm.es>
2020-04-11 13:07:17 +0200

    CRUD routes for the model Location

--- src/server/routers/admin.js
+++ src/server/routers/admin.js
@@ -1,5 +1,35 @@
 const express = require('express');
+const defaultController = require('../controllers/defaultController');
+const location = require('../controllers/location');
 
 const router = express.Router();
+
+// Autoload for routes using :param
+router.param(
+	'locationId',
+	defaultController.load(location.model, location.loadOptions),
+);
+
+// Routes for the model Location
+router.get(
+	'/locations',
+	location.setDefaults, defaultController.index,
+);
+router.get(
+	'/location/:locationId(\\d+)',
+	defaultController.show,
+);
+router.post(
+	'/location',
+	location.create, defaultController.create,
+);
+router.put(
+	'/location/:locationId(\\d+)',
+	location.update, defaultController.update,
+);
+router.delete(
+	'/location/:locationId(\\d+)',
+	location.setDefaults, defaultController.destroy,
+);
 
 module.exports = router;
