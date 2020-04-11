05f6532a84c7f25b55d50760826e8755c734e2a8
Enrique Pelaez <e.pelaez@alumnos.upm.es>
2020-04-11 12:57:32 +0200

    setDefaults, create and update settings for Location controller.

---
+++ src/server/controllers/location.js
@@ -0,0 +1,25 @@
+const models = require('../models');
+
+exports.model = models.Location;
+exports.loadOptions = {};
+
+exports.setDefaults = (req, res, next) => {
+	req.options = {};
+	req.model = models.Location;
+	next();
+};
+
+exports.update = (req, res, next) => {
+	req.allowedFields = ['name', 'description'];
+	next();
+};
+
+exports.create = (req, res, next) => {
+	req.entity = models.Location.build(
+		{
+			name: req.body.name,
+			description: req.body.description,
+		},
+	);
+	next();
+};
