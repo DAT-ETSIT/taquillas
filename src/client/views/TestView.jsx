d290690903bf9be30e087a1a839b40b811f04a27
Enrique Pelaez <e.pelaez@alumnos.upm.es>
2020-04-15 01:36:03 +0200

    Update the template to use hooks

--- src/client/views/TestView.jsx
+++ src/client/views/TestView.jsx
@@ -1,39 +1,24 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 import { fetchGet } from '../util';
 import { receivePong } from '../redux/actions/pong';
 
-const sendPing = function sendPing(props) {
-	fetchGet('/api/v1/ping')
-		.then((r) => r.json())
-		.then(() => props.dispatch(receivePong()));
+
+const TestView = () => {
+	const pong = useSelector((state) => state.pong);
+	const dispatch = useDispatch();
+	useEffect(() => {
+		fetchGet('/api/v1/ping')
+			.then((r) => r.json())
+			.then(() => dispatch(receivePong()));
+	});
+	return (
+		<div>
+			<h1>Hello, world!</h1>
+			{!pong ? 'Awaiting pong...' : 'Pong!'}
+		</div>
+	);
 };
 
-class TestView extends Component {
-	componentDidMount() {
-		sendPing(this.props);
-	}
-
-	componentDidUpdate() {
-		sendPing(this.props);
-	}
-
-	render() {
-		const { pong } = this.props;
-		return (
-			<div>
-				<h1>Hello, world!</h1>
-				{!pong ? 'Awaiting pong...' : 'Pong!'}
-			</div>
-		);
-	}
-}
-
-function mapStateToProps(state) {
-	return {
-		...state,
-	};
-}
-
-export default connect(mapStateToProps)(TestView);
+export default TestView;
