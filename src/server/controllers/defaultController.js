epelaez1/taquillas-1

Default destroy function

src/server/controllers/defaultController.js
@@ -47,3 +47,11 @@ exports.update = (req, res, next) => {
 		})
 		.catch((error) => next(error));
 };
+
+exports.destroy = (req, res, next) => {
+	req.entity.destroy()
+		.then(() => {
+			req.json({ status: 'OK' });
+		})
+		.catch((error) => next(error));
+};

DAT-ETSIT/taquillas

src/server/controllers/defaultController.js
@@ -47,3 +47,11 @@ exports.update = (req, res, next) => {
 		})
 		.catch((error) => next(error));
 };
+
+exports.destroy = (req, res, next) => {
+	req.entity.destroy()
+		.then(() => {
+			res.json({ status: 'OK' });
+		})
+		.catch((error) => next(error));
+};