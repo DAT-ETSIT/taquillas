34ff1ddadab20999409b38fa9a715ee48e56ab0f
Enrique Pelaez <e.pelaez@alumnos.upm.es>
2020-05-22 01:57:01 +0200

    Create migration with the session table

---
+++ src/server/migrations/20200513164548-session.js
@@ -0,0 +1,31 @@
+
+module.exports = {
+	up: (queryInterface, Sequelize) => queryInterface.createTable('Sessions',
+		{
+			sid: {
+				type: Sequelize.STRING,
+				allowNull: false,
+				primaryKey: true,
+				unique: true,
+			},
+			expires: {
+				type: Sequelize.DATE,
+			},
+			data: {
+				type: Sequelize.STRING(10000),
+			},
+			createdAt: {
+				type: Sequelize.DATE,
+				allowNull: false,
+			},
+			updatedAt: {
+				type: Sequelize.DATE,
+				allowNull: false,
+			},
+		},
+		{
+			sync: { force: true },
+		}),
+
+	down: (queryInterface) => queryInterface.dropTable('Sessions'),
+};
