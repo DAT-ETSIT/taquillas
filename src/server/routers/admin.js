0d54bf1b6afd0aa05ef5ea04ebdb6c490bad63b6
Enrique Pelaez <e.pelaez@alumnos.upm.es>
2020-05-21 09:47:19 +0200

    Rental CRUD Endpoints

--- src/server/routers/admin.js
+++ src/server/routers/admin.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const defaultController = require('../controllers/defaultController');
 const location = require('../controllers/admin/location');
+const rental = require('../controllers/admin/rental');
 
 const router = express.Router();
 
@@ -32,4 +33,32 @@ router.delete(
 	location.setDefaults, defaultController.destroy,
 );
 
+// Autoload for routes using :param
+router.param(
+	'rentalId',
+	defaultController.load(rental.model, rental.loadOptions),
+);
+
+// Routes for the model rental
+router.get(
+	'/rentals',
+	rental.index, defaultController.index,
+);
+router.get(
+	'/rental/:rentalId(\\d+)',
+	defaultController.show,
+);
+
+router.post(
+	'/rental',
+	rental.create, defaultController.create,
+);
+router.put(
+	'/rental/:rentalId(\\d+)',
+	rental.update, defaultController.update,
+);
+router.delete(
+	'/rental/:rentalId(\\d+)',
+	defaultController.destroy,
+);
 module.exports = router;
