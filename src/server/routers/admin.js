f94f0e1ba50350c51faabac136b43668ccc66e06
Enrique Pelaez <e.pelaez@alumnos.upm.es>
2020-05-21 09:24:07 +0200

    Payment CRUD Endpoints

--- src/server/routers/admin.js
+++ src/server/routers/admin.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const defaultController = require('../controllers/defaultController');
 const location = require('../controllers/admin/location');
+const payment = require('../controllers/admin/payment');
 
 const router = express.Router();
 
@@ -30,6 +31,34 @@ router.put(
 router.delete(
 	'/location/:locationId(\\d+)',
 	location.setDefaults, defaultController.destroy,
+);
+
+// Autoload for routes using :param
+router.param(
+	'paymentId',
+	defaultController.load(payment.model, payment.loadOptions),
+);
+
+// Routes for the model payment
+router.get(
+	'/payments',
+	payment.index, defaultController.index,
+);
+router.get(
+	'/payment/:paymentId(\\d+)',
+	defaultController.show,
+);
+router.post(
+	'/payment',
+	payment.create, defaultController.create,
+);
+router.put(
+	'/payment/:paymentId(\\d+)',
+	payment.update, defaultController.update,
+);
+router.delete(
+	'/payment/:paymentId(\\d+)',
+	defaultController.destroy,
 );
 
 module.exports = router;
