5a2f93e8b65670f704a85fe5b519fca058e5886e
Enrique Pelaez <e.pelaez@alumnos.upm.es>
2020-05-22 02:13:00 +0200

    Endpoints to manage the session (create, get and destroy).

--- src/server/routers/app.js
+++ src/server/routers/app.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const session = require('../controllers/app/session');
 
 const router = express.Router();
+
+// Session routes
+router.get('/session', session.index);
+router.get('/login', session.login);
+router.get('/session/new', session.casLogin, session.create);
+router.delete('/session', session.destroy);
 
 module.exports = router;
