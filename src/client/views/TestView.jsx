d878b690fd458974dd723ed53bbe3e8847fbf8be
Enrique Pelaez <e.pelaez@alumnos.upm.es>
2020-07-11 10:26:23 +0200

    Modify test view to see the changes

--- src/client/views/TestView.jsx
+++ src/client/views/TestView.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import doPing from '../utils/api/pong';
+import { logOut } from '../utils/api/app/session';
 
 
 const TestView = () => {
 	const pong = useSelector((state) => state.pong);
+	const session = useSelector((state) => state.session);
 	useEffect(() => {
 		doPing();
 	}, []);
@@ -12,6 +14,20 @@ const TestView = () => {
 		<div>
 			<h1>Hello, world!</h1>
 			{!pong ? 'Awaiting pong...' : 'Pong!'}
+			<br />
+			Logged in as: {session.user.name}
+			<br />
+			Email: {session.user.email}
+			<br />
+			Admin?: {session.user.isAdmin ? 'True' : 'False'}
+			<br />
+			<button
+				type="button"
+				onClick={() => logOut()}
+				style={{ marginLeft: '20px' }}
+			>
+				Cerrar Sesión
+			</button>
 		</div>
 	);
 };
