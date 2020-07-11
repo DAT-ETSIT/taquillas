ecf3380c6fbef3718a036c901ebdf680fc79cd90
Enrique Pelaez <e.pelaez@alumnos.upm.es>
2020-07-11 10:16:44 +0200

    Create the new user api endpoint and controller

--- src/server/routers/app.js
+++ src/server/routers/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const session = require('../controllers/app/session');
+const user = require('../controllers/app/user');
 
 const router = express.Router();
 
@@ -8,5 +9,8 @@ router.get('/session', session.index);
 router.get('/login', session.login);
 router.get('/session/new', session.casLogin, session.create);
 router.delete('/session', session.destroy);
+
+// User routes
+router.post('/user', user.create);
 
 module.exports = router;
