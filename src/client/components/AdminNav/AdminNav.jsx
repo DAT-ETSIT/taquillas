257d7ebccc1e99cb59fd6487235fd5d1a369de92
Enrique Pelaez <e.pelaez@alumnos.upm.es>
2020-07-24 11:22:07 +0200

    Design a navbar with links to the admin views

---
+++ src/client/components/AdminNav/AdminNav.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import NavLink from '../NavLink/NavLink';
+import './styles.css';
+
+const AdminNav = () => {
+	const location = useLocation();
+	return (
+		<nav>
+			<NavLink
+				location="/admin/requests"
+				text="Solicitudes"
+				isActive={location.pathname === '/admin/requests'}
+			/>
+			<NavLink
+				location="/admin/payments"
+				text="Pagos"
+				isActive={location.pathname === '/admin/payments'}
+			/>
+			<NavLink
+				location="/admin/rentals"
+				text="Préstamos"
+				isActive={location.pathname === '/admin/rentals'}
+			/>
+			<NavLink
+				location="/admin/users"
+				text="Usuarios"
+				isActive={location.pathname === '/admin/users'}
+			/>
+			<NavLink
+				location="/admin/lockers"
+				text="Taquillas"
+				isActive={location.pathname === '/admin/lockers'}
+			/>
+			<NavLink
+				location="/admin/locations"
+				text="Localizaciones"
+				isActive={location.pathname === '/admin/locations'}
+			/>
+		</nav>
+	);
+};
+
+export default AdminNav;
