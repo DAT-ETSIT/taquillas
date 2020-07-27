c5ecab30ea04b4ba999065b33404f5bcb3852862
Enrique Pelaez <e.pelaez@alumnos.upm.es>
2020-07-27 15:56:06 +0200

    Add the functions to request the rent of a locker in the rentals controller

---
+++ src/server/controllers/app/rental.js
@@ -0,0 +1,69 @@
+const models = require('../../models');
+const { LockerStates, RentalStates, MAX_RENTALS } = require('../../constants');
+const { NotFoundError, BadRequestError, LimitedUserError } = require('../../errors');
+
+exports.load = (req, res, next, rentalId) => {
+	models.Rental.findByPk(rentalId)
+		.then((rental) => {
+			if (rental) {
+				req.rental = rental;
+				next();
+			} else {
+				next(new NotFoundError());
+			}
+		})
+		.catch((error) => next(error));
+};
+
+exports.requestLocker = (req, res, next) => {
+	// If locker not available
+	if (req.locker.lockerStateId !== LockerStates.AVAILABLE) {
+		return next(new BadRequestError('La taquilla solicitada no se encuentra disponible'));
+	}
+	// If user with more than 5 active rentals
+	if (req.session.user.Rentals && req.session.user.Rentals.length >= MAX_RENTALS) {
+		return next(new LimitedUserError(`No puedes tener mas de ${MAX_RENTALS} alquileres activos`));
+	}
+	// req.locker.lockerStateId = LockerStates.RESERVED;
+	return req.locker.update({ lockerStateId: LockerStates.RESERVED })
+		.then((locker) => models.Rental.create(
+			{
+				expirationDate: Date.now(), // Deprecated?
+				deposit: 0,
+				userId: req.session.user.id,
+				lockerId: locker.id,
+				rentalStateId: RentalStates.REQUESTED,
+			},
+		)).then((rental) => {
+			req.result = rental;
+			next();
+		});
+};
+
+exports.requestRandomLocker = (req, res, next) => {
+	// If user with more than x active rentals
+	if (req.session.user.Rentals && req.session.user.Rentals.length >= MAX_RENTALS) {
+		return next(new LimitedUserError(`No puedes tener mas de ${MAX_RENTALS} alquileres activos`));
+	}
+	const options = {
+		where: {
+			locationId: req.location.id,
+			lockerStateId: LockerStates.AVAILABLE,
+		},
+	};
+
+	return models.Locker.findOne(options)
+		.then((locker) => locker.update({ lockerStateId: LockerStates.RESERVED }))
+		.then((locker) => models.Rental.create(
+			{
+				expirationDate: Date.now(), // Deprecated?
+				deposit: 0,
+				userId: req.session.user.id,
+				lockerId: locker.id,
+				rentalStateId: RentalStates.REQUESTED,
+			},
+		)).then((rental) => {
+			req.result = rental;
+			next();
+		});
+};
