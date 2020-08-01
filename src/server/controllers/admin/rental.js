550e882f01e0bd7f56cbee1c571b013e5f460cdc
Enrique Pelaez <e.pelaez@alumnos.upm.es>
2020-08-01 13:44:54 +0200

    Add a function in the rental controller to deny a request

--- src/server/controllers/admin/rental.js
+++ src/server/controllers/admin/rental.js
@@ -57,3 +57,12 @@ exports.acceptRequest = (req, res, next) => {
 		.then((rental) => res.json(rental));
 };
 
+exports.denyRequest = (req, res, next) => {
+	if (req.entity.rentalStateId !== RentalStates.REQUESTED) {
+		return next(new BadRequestError('No puedes denegar una solicitud de un alquiler que no se encuentre en estado "Solicitado"'));
+	}
+	return req.entity.Locker.update({ lockerStateId: LockerStates.AVAILABLE })
+		.then(() => req.entity.update({ rentalStateId: RentalStates.RETURNED }))
+		.then((rental) => rental.reload())
+		.then((rental) => res.json(rental));
+};
