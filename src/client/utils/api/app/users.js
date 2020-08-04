2cc487260711167161dbaea23aff24f194261be0
Enrique Pelaez <e.pelaez@alumnos.upm.es>
2020-08-04 21:47:25 +0200

    Add a function in the client API to update the user profile

--- src/client/utils/api/app/users.js
+++ src/client/utils/api/app/users.js
@@ -1,7 +1,11 @@
 import { fetchPost } from '../../asyncRequests';
 import { getSession } from './session';
 
-const createUser = (newUser) => fetchPost('/api/v1/app/user', newUser)
+export const createUser = (newUser) => fetchPost('/api/v1/app/user', newUser)
+	.then((r) => r.json())
+	.then(() => getSession());
+
+export const updateUser = (userId, userData) => fetchPost(`/api/v1/app/user/${userId}`, userData)
 	.then((r) => r.json())
 	.then(() => getSession());
 
