0191372077696ae5df26c6f98373a00ac79c9b48
Enrique Pelaez <e.pelaez@alumnos.upm.es>
2020-08-04 21:48:07 +0200

    Design the Profile view and add it to the AppRouter

---
+++ src/client/views/Profile/Profile.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import Card from '../../components/Card/Card';
+import EditProfile from '../../components/forms/EditProfile';
+import './styles.css';
+
+
+const Profile = (props) => {
+	const { user } = props;
+	const [isOpen, setIsOpen] = useState(false);
+
+	return (
+		<div className="profile">
+			<Card
+				name={user.name}
+				description={user.dni}
+				stats={{
+					name: 'E-mail',
+					description: user.email,
+				}}
+				actions={[
+					{
+						name: 'Modificar Datos',
+						handler: () => setIsOpen(true),
+					},
+				]}
+			/>
+			<EditProfile
+				isOpen={isOpen}
+				closeForm={() => setIsOpen(false)}
+				user={user}
+			/>
+		</div>
+	);
+};
+
+export default Profile;
