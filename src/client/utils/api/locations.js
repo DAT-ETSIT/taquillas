54ac3210587f144532130f86c400918f44c87397
Enrique Pelaez <e.pelaez@alumnos.upm.es>
2020-08-07 21:01:39 +0200

    Add the CRUD operations of the locations to the client API

--- src/client/utils/api/locations.js
+++ src/client/utils/api/locations.js
@@ -1,11 +1,31 @@
-import { fetchGet } from '../asyncRequests';
+import {
+	fetchGet, fetchPost,
+	fetchDelete, fetchPut,
+} from '../asyncRequests';
 import setLocations from '../../redux/actions/locations';
 import store from '../../redux/store';
 
 const { dispatch } = store;
 
-const getAvailableLocations = () => fetchGet('/api/v1/locations/available')
+export const getAvailableLocations = () => fetchGet('/api/v1/locations/available')
 	.then((r) => r.json())
 	.then((locations) => dispatch(setLocations(locations)));
 
-export default getAvailableLocations;
+export const getAllLocations = () => fetchGet('/api/v1/locations')
+	.then((r) => r.json());
+
+export const createLocation = (newLocation) => fetchPost('/api/v1/locations', newLocation)
+	.then((r) => r.json());
+
+export const updateLocation = (newLocation, oldLocation) => {
+	const url = `/api/v1/locations/${oldLocation.id}`;
+	return fetchPut(url, newLocation)
+		.then((r) => r.json())
+		.then((location) => location);
+};
+
+export const removeLocation = (location) => {
+	const url = `/api/v1/locations/${location.id}`;
+	return fetchDelete(url, {})
+		.then((r) => r.json());
+};
