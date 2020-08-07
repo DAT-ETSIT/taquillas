b6242499c22d9d134571bc2457bbf9df16e74343
Enrique Pelaez <e.pelaez@alumnos.upm.es>
2020-08-07 19:57:18 +0200

    Design a component to show all the data and actions in a table

---
+++ src/client/components/Table.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import MaterialTable from 'material-table';
+
+const Table = (props) => {
+	const {
+		data, title, columns, actions,
+		create, update, remove, editable,
+		showEmpty, detailPanel,
+	} = props;
+	const [filterActivated, setFilterActivated] = useState(false);
+	let triggers = {};
+	if (editable) {
+		triggers = {
+			onRowAdd: (newData) => create(newData),
+			onRowUpdate: (newData, oldData) => update(newData, oldData),
+			onRowDelete: (oldData) => remove(oldData),
+		};
+	}
+	if (data.length === 0 && !showEmpty) {
+		return (
+			<div>
+				<hr />
+				<div style={{ textAlign: 'center', fontStyle: 'italic', fontWeight: 'bold' }}> No hay {title.toLowerCase()} </div>
+				<hr />
+			</div>
+		);
+	}
+	return (
+		<MaterialTable
+			title={title}
+			columns={columns}
+			data={data}
+			editable={triggers}
+			options={{
+				filtering: filterActivated,
+				selection: true,
+			}}
+			detailPanel={detailPanel}
+			actions={[
+				...actions,
+				{
+					icon: 'filter_list',
+					tooltip: 'Filtrar datos',
+					onClick: () => setFilterActivated(!filterActivated),
+					position: 'toolbar',
+				},
+			]}
+		/>
+	);
+};
+
+export default Table;
