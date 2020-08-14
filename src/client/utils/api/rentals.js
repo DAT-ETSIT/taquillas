01789920f0d7a9a62aabdd7379c05e5788fb644c
Enrique Pelaez <e.pelaez@alumnos.upm.es>
2020-08-14 10:27:03 +0200

    Refactor the routes and names related with the rentals

--- src/client/utils/api/rentals.js
+++ src/client/utils/api/rentals.js
@@ -1,8 +1,8 @@
-import { fetchPost } from '../../asyncRequests';
+import { fetchPost } from '../asyncRequests';
 
 const requestLocker = (locationId, lockerId) => {
-	const requestLockerUrl = `/api/v1/app/request/locker/${lockerId}`;
-	const requestAnyUrl = `/api/v1/app/request/any/${locationId}`;
+	const requestLockerUrl = `/api/v1/rentals/request/locker/${lockerId}`;
+	const requestAnyUrl = `/api/v1/rentals/request/any/${locationId}`;
 	const url = lockerId >= 0 ? requestLockerUrl : requestAnyUrl;
 	return fetchPost(url)
 		.then((r) => r.json());
