250292fbc4f16ea56be4e252f3477ce657edad8a
Enrique Pelaez <e.pelaez@alumnos.upm.es>
2021-04-11 20:55:20 +0200

    Add rental actions to the client API

--- src/client/utils/api/rentals.js
+++ src/client/utils/api/rentals.js
@@ -1,10 +1,61 @@
-import { fetchPost } from '../asyncRequests';
+import {
+	fetchGet, fetchPost,
+	fetchPut, fetchDelete,
+} from '../asyncRequests';
 
 const requestLocker = (locationId, lockerId) => {
 	const requestLockerUrl = `/api/v1/rentals/request/locker/${lockerId}`;
 	const requestAnyUrl = `/api/v1/rentals/request/any/${locationId}`;
 	const url = lockerId >= 0 ? requestLockerUrl : requestAnyUrl;
 	return fetchPost(url)
+		.then((r) => r.json());
+};
+
+export const getAllRentals = () => fetchGet('/api/v1/rentals')
+	.then((r) => r.json());
+
+export const createRental = (newRental) => fetchPost('/api/v1/rentals', newRental)
+	.then((r) => r.json());
+
+export const updateRental = (newRental, oldRental) => {
+	const url = `/api/v1/rentals/${oldRental.id}`;
+	return fetchPut(url, newRental)
+		.then((r) => r.json());
+};
+
+export const removeRental = (rental) => {
+	const url = `/api/v1/rentals/${rental.id}`;
+	return fetchDelete(url, {})
+		.then((r) => r.json());
+};
+
+export const requestRenewal = (rental) => {
+	const url = `/api/v1/rentals/${rental.id}/request/renewal/`;
+	return fetchPost(url, rental)
+		.then((r) => r.json());
+};
+
+export const claimRental = (rental) => {
+	const url = `/api/v1/rentals/${rental.id}/claim`;
+	return fetchPost(url, rental)
+		.then((r) => r.json());
+};
+
+export const acceptRenewal = (rental) => {
+	const url = `/api/v1/rentals/${rental.id}/renewal/accept`;
+	return fetchPost(url, rental)
+		.then((r) => r.json());
+};
+
+export const denyRenewal = (rental) => {
+	const url = `/api/v1/rentals/${rental.id}/renewal/deny`;
+	return fetchPost(url, rental)
+		.then((r) => r.json());
+};
+
+export const endRental = (rental) => {
+	const url = `/api/v1/rentals/${rental.id}/end`;
+	return fetchPost(url, rental)
 		.then((r) => r.json());
 };
 
